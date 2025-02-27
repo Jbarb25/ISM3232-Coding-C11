4918033b143ab652a9ca43088e2574f41b6cf947
Jbarb25 <barboza205@usf.edu>
2025-02-26 19:31:13 -0500

    Task 4 - Implemented Book Borrowing
    Add a method to lend books to borrowers by finding the borrower exists in the borrower array, finding the book exists in the book array using id, and updating the copies available for the book. We also assign the book to the borrower's list of borrowed books.

--- cc_11.js
+++ cc_11.js
@@ -32,17 +32,16 @@ class Borrower {  //Create a class Borrower to store information about books che
         this.borrowedBooks = [];}
 
     borrowBook(book){
-        this.borrowedBooks.push(book); //method that adds the book plugged in when called to the borrowered books array
-        console.log(`${this.name} checked out ${book}`);}
+        this.borrowedBooks.push(book.title);} //method that adds the book plugged in when called to the borrowered books array
         
     returnBook(book){
-        this.borrowedBooks.pop(book);  //mehtod that removes the book plugged in when called from the array meaning it is returned
-        console.log(`${book} was returned by ${this.name}`);}
+        this.borrowedBooks.pop(book);}  //mehtod that removes the book plugged in when called from the array meaning it is returned
+
 }
 
 //Establish a new borrower with a book they are checking out 
 const borrower1 = new Borrower("Alice Johnson", 201);
-borrower1.borrowBook("The Great Gatsby"); //assign the book they are checking out to their name
+borrower1.borrowBook(book1); //assign the book they are checking out to their name
 console.log(borrower1.borrowedBooks);  //print the book in the array
 
 borrower1.returnBook("The Great Gatsby");  //remove the book from the array using the method
@@ -51,18 +50,43 @@ console.log(borrower1.borrowedBooks); //print an updated array list
 
 //Task 3: Creating a library class
 class Library {
-    constructor(books, borrowers){  //create a libabry class that stores the list of books in the library and the list of borrowers for the books
+    constructor(books, borrowers){  //create a library class that stores the list of books in the library and the list of borrowers for the books
         this.books = [];
         this.borrowers = [];}
 
     addBook(book){
         return this.books.push(book);}  //add a method that adds a book to the library array of books
+    
+    addBorrower(borrower){
+        return this.borrowers.push(borrower);}
 
     listBooks(){
         return this.books.forEach(book => console.log(book.getDetails()));} //print the updated information for the updated list of books in the library 
-}
 
+    //Task 4: Implementing Book Borrowing
+
+    lendBook(borrowerId, isbn){  //Create a method that lends a book to a borrower from the library
+        const borrower = this.borrowers.find(b => b.borrowerId === borrowerId); //for borrower find the borrower ID
+        const book = this.books.find(b => b.isbn === isbn); //for book find the isbn
+
+        if (!borrower){  //if the borrower is not found by the borrowerID, do the following
+            console.log('Borrower not found');}  //error message
+
+        if (!book || book.copies <= 0){  //if the book does not exist or there are no copies, do the following
+        console.log('Not enough copies of Book');} //error message  
+        
+        borrower.borrowBook(book); //update the borrowed books list from the Borrower class to inclue the new lended book
+        book.updateCopies(-1);} //remove one copy from the number of copies for the book from the library
+
+}
 //Estabish a new library
 const library = new Library();
 library.addBook(book1);  //add a new book to the array of books in the library
 library.listBooks();  //print the updated information of each book in the library
+
+
+//Task 4: Implementing Book Borrowing
+library.addBorrower(borrower1); //add a new borrower to the borrower list
+library.lendBook(201, 123456); //lend a book using the lend book method plugging in the parameters
+console.log(book1.getDetails()); //print the updated details for the book
+console.log(borrower1.borrowedBooks); //print the borrowered books assigned to the borrower 
